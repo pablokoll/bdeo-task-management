40610a7cb51ac1a219b2cb10871c3e5ef00fb386
Pablo Koll Ideapad <pablokollm@gmail.com>
2024-06-28 15:36:37 +0200

    add tasks module

---
+++ api/test/tasks/tasks.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, type TestingModule } from '@nestjs/testng';
+import { TasksController } from '../../src/tasks/tasks.controller';
+
+describe('TasksController', () => {
+  let controller: TasksController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TasksController],
+    }).compile();
+
+    controller = module.get<TasksController>(TasksController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
