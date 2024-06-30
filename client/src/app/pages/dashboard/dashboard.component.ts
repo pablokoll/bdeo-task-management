7a2b05c2cf7ced80f8f49ca948173422212d05dc
Pablo Koll Ideapad <pablokollm@gmail.com>
2024-07-01 00:44:40 +0200

    add dashboard and not found component

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CreateTaskComponent } from '../../components/create-task/create-task.component';
+import { TaskStatusListComponent } from '../../components/task-status-list/task-status-list.component';
+import { CreateTaskDto } from '../../shared/dto/create-task.dto';
+import { TaskStatus } from '../../shared/enum/task-status.enum';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TaskStatusListComponent, CreateTaskComponent, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  showPopupCreateTask = false;
 
+  openCreateTaskPopup(): void {
+    this.showPopupCreateTask = true;
+  }
+  
+  onClosePopup(): void {
+    this.showPopupCreateTask = false;
+  }
+
+  onTaskCreated(task: CreateTaskDto): void {
+    console.log('Task created:', task);
+    this.showPopupCreateTask = false;
+  }
+  status = Object.keys(TaskStatus).map((status, index) => {
+    return {
+      status,
+      index,
+    };
+  });
 }
