1ea43550a90ac90f0e0a6eaf6da3f4556dae0c12
Pablo Koll Ideapad <pablokollm@gmail.com>
2024-06-30 18:26:53 +0200

    add task model

--- client/src/app/models/task.model.ts
+++ client/src/app/models/task.model.ts
@@ -1,3 +1,11 @@
-export class Task {
-    
-}
+import { TaskStatus } from "../shared/enum/task-status.enum";
+
+export class Task{
+    constructor(
+      public _id: string,
+      public title: string,
+      public description: string,
+      public status: TaskStatus,
+      public __v: number,
+    ){}
+  }
---
+++ client/src/app/shared/enum/task-status.enum.ts
@@ -0,0 +1,5 @@
+export enum TaskStatus {
+    'TO-DO' = 'to-do',
+    'IN-PROGRESS' = 'in-progress',
+    DONE = 'done',
+  }
