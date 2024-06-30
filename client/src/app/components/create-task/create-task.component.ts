234adef1611b929b635b178caca6bd411e83a318
Pablo Koll Ideapad <pablokollm@gmail.com>
2024-07-01 00:49:25 +0200

    add create task component

--- client/src/app/components/create-task/create-task.component.ts
+++ client/src/app/components/create-task/create-task.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-task',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-task.component.html',
-  styleUrl: './create-task.component.css'
+  styleUrl: './create-task.component.css',
 })
-export class CreateTaskComponent {
+export class CreateTaskComponent implements OnInit {
+  ngOnInit(): void {}
 
+  @Output() formClosed = new EventEmitter<void>();
+  @Output() taskCreated = new EventEmitter<any>();
+  @Output() closePopup = new EventEmitter<void>();
+
+  taskForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.min(3),
+      Validators.max(30),
+    ]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.min(5),
+      Validators.max(200),
+    ]),
+  });
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      this.taskCreated.emit(this.taskForm.value);
+      this.onCancel();
+      this.taskForm.reset();
+    }
+  }
+
+  onCancel(): void {
+    this.closePopup.emit();
+  }
 }
---
+++ client/src/app/shared/dto/create-task.dto.ts
@@ -0,0 +1,3 @@
+export class CreateTaskDto {
+  constructor(public _id: string, public title: string) {}
+}
